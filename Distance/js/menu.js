b42e9090a973bb77aed71d5e058a3d0fa6f652dd
Capitan111 <45821350+Capitan111@users.noreply.github.com>
2020-06-05 16:36:55 +0600

    Add files via upload

---
+++ Distance/js/menu.js
@@ -0,0 +1,6 @@
+    const Menubtn = document.querySelector('.m-menu');
+    const menu = document.querySelector('.menu-sidebar');
+
+    Menubtn.addEventListener('click',function(){
+        menu.classList.toggle('active');
+});
